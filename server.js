61da59e92f6e1c85549eead7f82817d3e206d336
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-12-18 01:38:56 -0800

    filled out API route logic with pseudocode

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+// const fs = require("")
+const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -10,20 +11,39 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-//Stored notes array
-var notes = [];
-
 // Routes
 //API routes
 app.get("/api/notes", function(req, res) {
-  return res.json(notes);
+  //Use the fs module to read the file
+  //THEN parse the file contents with JSON parse to get the real data 
+  
+  // return res.json(notes); ==> send the parsed data to the client with res.json
 });
+
+app.post("/api/notes", function(req, res) {
+  // Access the posted data in req.body
+
+  //use the fs module to read the file
+  //then parse the file contents with JSON.parse() to the real data
+  //Push req.body to the array list.
+  //JSON.stringify() the array list back into JSON string
+  //Save the content back to the db.json file with the fs module
+
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  // Acces :id from 'req.params.id'
+  //Use the fs module to read the file
+  //THEN parse the file contents with JSON parse to get the real data 
+  
+  //Find the matching index using .findIndex()
+  //Remove the target element using .splice()
+
+  // Return any type of success message
+
+})
 
 //HTML Routes
-app.get("/", function(req, res) {
-  res.send("Hey!");
-});
-
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
